1fe89607b72419426f7ee4c9999bee29ffe97903
AndHol996 <andrewholland2000@gmail.com>
2024-09-05 21:02:43 -0400

    Modified the outputs of main function

--- customer_banking.py
+++ customer_banking.py
@@ -11,29 +11,29 @@ def main():
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     savings_balance=float(input("How much are you wanting to put into saving?\n "))
     savings_interest=float(input("What is the APR of the savings account?\n "))
-    savings_maturity=int(input("How amany months will it stay in this account?\n "))
+    savings_maturity=int(input("How many months will it stay in this account?\n "))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, s_interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account ba1lance with interest earned for the given months.
     print(f"""After {savings_maturity} months,
-The interest earned will be ${s_interest_earned:.2f}.
-The total balance will be ${updated_savings_balance:.2f} in the account.""")
+The interest earned will be ${s_interest_earned:,.2f}.
+The total balance will be ${updated_savings_balance:,.2f} in the account.""")
     
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     
     cd_balance=float(input("How much are you wanting to put into a CD?\n "))
     cd_interest=float(input("What is the APR of the CD account?\n "))
-    cd_maturity=int(input("How amany months will it stay in this account?\n "))
+    cd_maturity=int(input("How many months will it stay in this account?\n "))
     
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, c_interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     print(f"""After {cd_maturity} months,
-The interest earned will be ${c_interest_earned:.2f}.
-The total balance will be ${updated_cd_balance:.2f} in the account.""")
+The interest earned will be ${c_interest_earned:,.2f}.
+The total balance will be ${updated_cd_balance:,.2f} in the account.""")
 
 if __name__ == "__main__":
     
